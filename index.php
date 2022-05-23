7ca2ae2e5609d0b8463f7674f878ad7d1a92fb40
robertolatini31 <robertolatini31@gmail.com>
2022-05-23 14:23:02 +0200

    add header

--- index.php
+++ index.php
@@ -12,8 +12,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Domande frequenti - Privacy & Terms</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
+
+<header class="py-3 d-flex flex-column justify-content-center">
+    <div class="px-3 top_header d-flex align-items-center mb-3">
+        <img class="logo_img me-2" src="./assets/img/logo.png" alt="logo-google">
+        <h3 class="text-secondary">Privacy & Termini</h3>
+    </div>
+    <!-- /.top_header -->
+    <div class="bottom_header">
+        <ul class="d-flex m-0 p-0 px-3 border-bottom border-secondary">
+            <li class="me-3 pb-2"><a href="#">Introduzione</a></li>
+            <li class="me-3 pb-2"><a href="#">Norme sulla privacy</a></li>
+            <li class="me-3 pb-2"><a href="#">Termini di servizio</a></li>
+            <li class="me-3 pb-2"><a href="#">Tecnologie</a></li>
+            <li class="me-3 pb-2 active border-bottom border-2 border-primary"><a href="#" class="text-primary">Domande frequenti</a></li>
+        </ul>
+    </div>
+    <!-- /.bottom_header -->
+</header>
+
     
 </body>
 </html>
